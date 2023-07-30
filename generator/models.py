eaa83a42a727638cf2374489cd2d3e8e59d545dc
sgierka <shadogry@gmail.com>
2023-07-30 20:37:24 +0200

    import data to database

--- generator/models.py
+++ generator/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Word(models.Model):
-    word = models.CharField(max_length=50, unique=True)
+    word = models.CharField(max_length=50)
 
     def __str__(self):
         return self.word
@@ -11,7 +11,8 @@ class Word(models.Model):
 
 class Meaning(models.Model):
     word = models.ForeignKey(Word, on_delete=models.CASCADE)
-    meaning = models.CharField(max_length=100)
+    meaning_id = models.AutoField(primary_key=True)
+    meaning = models.TextField()
 
     def __str__(self):
         return self.meaning
